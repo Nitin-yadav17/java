f0e2aee9c5693c8bc5b804bb6dd33b03f2151e22
Nitin-yadav17 <nitinyadav70707@gmail.com>
2020-10-01 10:47:15 +0530

    Create number.java
    program to print a number

---
+++ number.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class first{
+ public static void main(String[] args) {
+   Scanner sc=new Scanner(System.in);
+   int t;
+   System.out.println("enter a number");
+   t=sc.nextInt();
+   System.out.println(t);
+   sc.close();
+  }
+}
